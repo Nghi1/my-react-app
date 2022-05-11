a13fde0df7acb4e638c2457fcafd27d6147d6f13
Nghi1 <nghivu201@gmail.com>
2022-05-11 15:04:35 +0700

    edit css and setTimeout()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+/*import './App.css';
 
 function App() {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const temperature=()=>{
+    if(true)
+    return(<img id="temp" width={50} height={50} src='https://previews.123rf.com/images/imagevectors/imagevectors1601/imagevectors160100222/50599125-flat-green-temperature-icon-and-green-circle.jpg'/>)
+  else{
+    return(<img id="temp" width={50} height={50} src='https://w7.pngwing.com/pngs/222/641/png-transparent-computer-icons-temperature-miscellaneous-photography-presentation.png'/>)
+  }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    console.log(getRandomInt(3)),
+      temperature()
   );
 }
 
+export default App;
+*/
+import React, {Component} from 'react';
+import "./App.css";
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timePassed: false
+    };
+  }
+  render() {
+     setTimeout(() => {this.setState({timePassed: true})}, 1700);
+    if (!this.state.timePassed){
+      return (
+        <img id="temp" width={50} height={50} src='https://previews.123rf.com/images/imagevectors/imagevectors1601/imagevectors160100222/50599125-flat-green-temperature-icon-and-green-circle.jpg'/>
+        );
+    }else{
+      return (
+        <img id="temp" width={50} height={50} src='https://w7.pngwing.com/pngs/222/641/png-transparent-computer-icons-temperature-miscellaneous-photography-presentation.png'/>      
+      );
+  }
+  }
+}
 export default App;
