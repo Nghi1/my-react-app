41bdee43d5bda491370ce5e48b16bcc23d30f13a
Nghi1 <nghivu201@gmail.com>
2022-05-12 23:06:41 +0700

    add camera

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-/*import './App.css';
-
-function App() {
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  const temperature=()=>{
-    if(true)
-    return(<img id="temp" width={50} height={50} src='https://previews.123rf.com/images/imagevectors/imagevectors1601/imagevectors160100222/50599125-flat-green-temperature-icon-and-green-circle.jpg'/>)
-  else{
-    return(<img id="temp" width={50} height={50} src='https://w7.pngwing.com/pngs/222/641/png-transparent-computer-icons-temperature-miscellaneous-photography-presentation.png'/>)
-  }
-  }
-  return (
-    console.log(getRandomInt(3)),
-      temperature()
-  );
-}
-
-export default App;
-*/
 import React, {Component} from 'react';
 import "./App.css";
 
@@ -33,12 +12,21 @@ class App extends Component {
   render() {
     const VongLap=()=>{
        if (!this.state.timePassed){
-      return (
-        <img id="temp" width={50} height={50} src='https://previews.123rf.com/images/imagevectors/imagevectors1601/imagevectors160100222/50599125-flat-green-temperature-icon-and-green-circle.jpg'/>
+      return (<html><img id="cam" width={50} height={50} src='https://viliho.com/wp-content/uploads/2016/08/security-camera-xxl.png'/><br></br>
+             <img id="cam2" width={50} height={50} src='https://media.istockphoto.com/vectors/surveillance-camera-icon-vector-id1138989739?k=20&m=1138989739&s=170667a&w=0&h=JheQCUKP4biUihsaMfOyuyFOi7FGfELSQClDlZgSkHg='/><br></br>
+             <img id="cam1" width={50} height={50} src='https://cdn2.vectorstock.com/i/1000x1000/57/51/security-camera-dome-icon-in-flat-style-for-apps-vector-25915751.jpg'/>
+             
+            </html>
         );
     }else{
       return (
-        <img id="temp" width={50} height={50} src='https://w7.pngwing.com/pngs/222/641/png-transparent-computer-icons-temperature-miscellaneous-photography-presentation.png'/>      
+          <html>
+             <img id="cam" width={50} height={50} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_hynrQl1MA1n3lsoH0R4MOpLbkhe810xWqg&usqp=CAU'/>
+             <br></br>
+            <img id="cam2" width={50} height={50} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl8lf_44fSbM3_mRQ85DPGv5vpady1KzIRMwjAEWQHaE1tTXRbjQbV5SRB7KFgUuWbxXQ&usqp=CAU'/><br></br>
+            <img id="cam1" width={50} height={50} src='https://cdn1.vectorstock.com/i/1000x1000/52/25/security-camera-dome-red-icon-on-white-background-vector-27785225.jpg'/>
+               
+          </html>
         );
   }
     }
